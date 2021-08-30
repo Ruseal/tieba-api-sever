ca041192290d83219fddbaa46d30b1006178d6fb
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-08-30 16:17:26 +0800

    完成用户登入返回token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,9 @@
 // 逻辑操作数据返回模块
+const jwt = require('jsonwebtoken')
 
 const userService = require('../service/user.service')
 const errorType = require('../constents/error-type-text')
+const { PRIVATE_KEY } = require('../app/config')
 
 let result = null
 
@@ -20,6 +22,23 @@ class UserController {
       msg: '注册成功',
       id: result.insertId,
       name
+    }
+  }
+
+  //用户登入返回token
+  login(ctx) {
+    const { id, name } = ctx.user
+    //通过jwt生成token
+    const token = jwt.sign({ id, name }, PRIVATE_KEY, {
+      expiresIn: 10,        //设置几秒后token失效
+      algorithm: 'RS256'    //设置编码token时候的算法
+    })
+    ctx.body = {
+      status: 200,
+      message: '登入成功',
+      id,
+      name,
+      token                      //返回token给前端
     }
   }
 }
