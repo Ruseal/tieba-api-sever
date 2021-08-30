f1209b91551312015adb2fc352901a7f1e66c265
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-08-30 11:50:04 +0800

    添加用户登入请求

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -3,16 +3,19 @@ const Router = require('koa-router')
 //验证处理层导入
 const {
   verifyRegister,
-  encryptedPassword
+  encryptedPassword,
+  verifyLogin
 } = require('../middleware/user.middleware')
 //逻辑操作层导入
 const {
-  register
+  register,
+  login
 } = require('../controller/user.controller')
 
 
 const userRouter = new Router({ prefix: '/user' })   //创建路由实例，并且定义路由前缀地址
 
 userRouter.post('/register', verifyRegister, encryptedPassword, register)
+userRouter.post('login',verifyLogin,login)
 
 module.exports = userRouter
