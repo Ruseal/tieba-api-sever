6ac13c8d91d493f77b30bbe9354a7f98c409553e
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-08-30 16:15:43 +0800

    读取token加密文件

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,12 @@
 // 获取.env文件常量并进行处理
 
 const dotenv = require('dotenv')
+const fs = require('fs')
+const path = require('path')
+
+//读取token加密文件
+const PRIVATE_KEY = fs.readFileSync(path.resolve(__dirname, './keys/private.key'))
+const PUBLIC_KEY = fs.readFileSync(path.resolve(__dirname, './keys/public.key'))
 
 dotenv.config()
 
@@ -18,3 +24,6 @@ module.exports = {
   MIN_PASSWORD_LENGTH,
   MAX_PASSWORD_LENGTH
 } = process.env
+
+module.exports.PRIVATE_KEY = PRIVATE_KEY
+module.exports.PUBLIC_KEY = PUBLIC_KEY
