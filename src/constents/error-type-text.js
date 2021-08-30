906ed0e819d28da22cdff67196736012ba748c31
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-08-30 16:13:55 +0800

    添加错误常量信息

--- src/constents/error-type-text.js
+++ src/constents/error-type-text.js
@@ -3,10 +3,14 @@
 const USER_OR_PASSWORD_RULE_IS_INCORRECT = 'account_or_password_rule_is_incorrect'
 const USER_ALREADY_EXISTS = 'user_already_exists'
 const SERVER_ERROR = 'server_error'
+const USER_DOES_NOT_EXISTS = 'user_name_does_not_exists'
+const PASSWORD_MISTAKE = 'password_mistake'
 
 
 module.exports = {
   USER_OR_PASSWORD_RULE_IS_INCORRECT,
   USER_ALREADY_EXISTS,
-  SERVER_ERROR
+  SERVER_ERROR,
+  USER_DOES_NOT_EXISTS,
+  PASSWORD_MISTAKE
 }
