55cf5a860d0c4355eea14b18a18a08d2681de363
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-08-30 16:04:07 +0800

    添加用户登入验证

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,21 +1,29 @@
-// user验证处理模块
+/*  
+ *     user验证处理模块 
+*/
 
 const errorType = require('../constents/error-type-text')
-const config = require('../app/config')
 const userService = require('../service/user.service')
 const md5password = require('../utils/encrypted-password')
+
+//定义正则
+const nameReg = /^[\w\u4e00-\u9fa5-@=.~、]{1,8}$/
+const pwReg = /^(?![_.])[\w.]{6,16}$/
+
+//密码格式校验
+const fmCheck = (name, password) => {
+  return !nameReg.test(name) || !pwReg.test(password)
+}
 
 // 验证注册
 const verifyRegister = async (ctx, next) => {
   const { name, password } = ctx.request.body
-
-  if (!name || name.length > config.NAME_LENGTH ||
-    !password || password.length > config.MAX_PASSWORD_LENGTH ||
-    password.length < config.MIN_PASSWORD_LENGTH) {
+  // ---------------格式校验-------------
+  if (fmCheck(name, password)) {
     const error = new Error(errorType.USER_OR_PASSWORD_RULE_IS_INCORRECT)
     return ctx.app.emit('error', error, ctx)                    //发射“error”监并且传入错误信息和context实例
   }
-
+  // ----------------验证是否不存在该用户名-----------
   try {
     const userResult = await userService.isUseByName(name)
     if (userResult.length) {
@@ -37,7 +45,41 @@ const encryptedPassword = async (ctx, next) => {
   await next()
 }
 
+// 验证登入
+const verifyLogin = async (ctx, next) => {
+  const { name, password } = ctx.request.body
+  // --------------------格式校验----------------
+  if (fmCheck(name, password)) {
+    const error = new Error(errorType.USER_OR_PASSWORD_RULE_IS_INCORRECT)
+    return ctx.app.emit('error', error, ctx)                    //发射“error”监并且传入错误信息和context实例
+  }
+
+  let ctxUser = {}
+  // ---------------------验证是否存在该用户------------------
+  try {
+    const userResult = await userService.isUseByName(name)
+    if (!userResult.length) {
+      const error = new Error(errorType.USER_DOES_NOT_EXISTS)
+      return ctx.app.emit('error', error, ctx)
+    }
+    ctxUser = userResult[0]
+  } catch (e) {
+    const error = new Error(errorType.SERVER_ERROR)
+    return ctx.app.emit('error', error, ctx)
+  }
+  // -----------------验证密码是否正确------------------
+  if (ctxUser.password !== md5password(password)) {
+    const error = new Error(errorType.PASSWORD_MISTAKE)       //创建错误信息实例
+    return ctx.app.emit('error', error, ctx)                    //发射“error”监并且传入错误信息和context实例
+  }
+  
+  ctx.user = ctxUser
+  await next()
+}
+
+
 module.exports = {
   verifyRegister,
-  encryptedPassword
+  encryptedPassword,
+  verifyLogin
 }
