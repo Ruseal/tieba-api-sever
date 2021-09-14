d270f5e8ae4a6e9ce55d2b97c7af16e90263b0b5
Ruseal <9441461+ruseal@user.noreply.gitee.com>
2021-09-14 14:38:21 +0800

    commit config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // 程序入口
 
-require('./app/database')    
+require('./app/database')
 const app = require('./app')
 const config = require('./app/config')
+
+
 
 app.listen(config.APP_PORT, () => {
   console.log(`启动端口${config.APP_PORT}服务器成功`);
